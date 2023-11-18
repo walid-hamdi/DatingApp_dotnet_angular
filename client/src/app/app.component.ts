4598d7fa97ae6c4d75ef8d1400cc4e798f282d75
walid-hamdi <walidhamdidev@gmail.com>
2023-11-18 22:22:55 +0100

    Listing the users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+interface User {
+  id: number;
+  userName: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers() {
+    return this.http
+      .get<User[]>('https://localhost:5001/api/users')
+      .subscribe((users) => {
+        this.users = users;
+      });
+  }
 }
