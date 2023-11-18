5a1b7c82ccaa2b1b0c63691dff4aaf19a9df88d7
walid-hamdi <walidhamdidev@gmail.com>
2023-11-18 22:22:32 +0100

    Add cors to allow front app to use this api

--- API/Program.cs
+++ API/Program.cs
@@ -2,16 +2,29 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>((opt) =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnectionString"));
+
 });
 
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(name: "MyPolicy",
+//                       builder =>
+//                       {
+//                           builder.WithOrigins("http://localhost:4200");
+//                       });
+// });
+
 var app = builder.Build();
+
+app.UseCors(builder =>
+                      {
+                          builder.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200");
+                      });
 
 app.MapControllers();
 
