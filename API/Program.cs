c779fb01bb505c64d6619177bcf9073b5214d7dc
walid-hamdi <walidhamdidev@gmail.com>
2023-11-21 07:59:54 +0100

    Adding the creation token service

--- API/Program.cs
+++ API/Program.cs
@@ -1,31 +1,17 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>((opt) =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnectionString"));
-
-});
-
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy(name: "MyPolicy",
-//                       builder =>
-//                       {
-//                           builder.WithOrigins("http://localhost:4200");
-//                       });
-// });
+builder.Services.AddDbContext<DataContext>((opt) =>{ opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnectionString")); });
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
-app.UseCors(builder =>
-                      {
-                          builder.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200");
-                      });
-
+app.UseCors(builder => { builder.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200"); });
 app.MapControllers();
 
 app.Run();
