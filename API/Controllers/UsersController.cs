fc5a8bc90459ddf16acfbdf7c118560f5b36cc67
walid-hamdi <walidhamdidev@gmail.com>
2023-12-07 08:43:49 +0100

    Presisting the changes in the api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -36,6 +37,21 @@ namespace API.Controllers
             var user = await _userRepository.GetMemberAsync(username);
             // return _mapper.Map<MemberDto>(user);
             return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user.");
         }
 
 
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,13 +14,17 @@ namespace API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>().
+
             ForMember(dest => dest.PhotoUrl, opt =>
              opt.MapFrom(src =>
             src.Photos.FirstOrDefault(x => x.IsMain).Url))
+
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
             src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotoDto>();
+
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
