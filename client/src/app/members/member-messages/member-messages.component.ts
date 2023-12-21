a715737d064095e07510f07889a84740cc706b44
walid-hamdi <walidhamdidev@gmail.com>
2023-12-21 10:57:26 +0100

    Sending message

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, ViewChild, inject } from '@angular/core';
 import { TimeagoModule } from 'ngx-timeago';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,6 +7,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 
 import { Message } from '../../model/message';
+import { MessageService } from '../../services/message.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
@@ -18,11 +20,24 @@ import { Message } from '../../model/message';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
+    FormsModule,
   ],
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css',
 })
 export class MemberMessagesComponent {
+  @ViewChild('messageForm') messageForm?: NgForm;
   @Input() messages: Message[] = [];
   @Input() username?: string;
+  messageContent?: string;
+  messageService = inject(MessageService);
+
+  sendMessage() {
+    this.messageService
+      .sendMessage(this.username!, this.messageContent!)
+      .subscribe((message: any) => {
+        this.messages.push(message);
+        this.messageForm!.reset();
+      });
+  }
 }
